c68cb96ed3ce87f05a5f7e9d3ce30e180be55f32
arturzia <38842268+arturzia@users.noreply.github.com>
2018-09-07 22:49:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://desolate-brushlands-32372.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://api.trello.com/1/v1/trello/';
+  const trelloApiRoot = 'https://desolate-brushlands-32372.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
