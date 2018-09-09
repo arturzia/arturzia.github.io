320311a28ebfce5550068d3457c70b205ffc7795
arturzia <38842268+arturzia@users.noreply.github.com>
2018-09-09 12:37:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'https://shielded-earth-64960.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://shielded-earth-64960.herokuapp.com/v1/trello/';
+  const apiRoot = 'https://afternoon-meadow-66225.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://afternoon-meadow-66225.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
